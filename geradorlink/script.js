d3c01e98900a362ad90aecb8016f6763cacde982
OttGod12 <130059670+OttGod12@users.noreply.github.com>
2024-05-14 23:04:32 -0300

    .

---
+++ geradorlink/script.js
@@ -0,0 +1,13 @@
+const btnCopiar = document.querySelector("[btnCopiar]");
+const btnMandar = document.querySelector("[btnMandar]");
+const numeroTel = document.querySelector("[numeroTel]");
+const resultado = document.querySelector("[inputResult]");
+
+
+btnCopiar.addEventListener("click", () => {
+    console.log("copiar")
+  });
+
+btnMandar.addEventListener("click", () => {
+    console.log("mandar")
+  });
