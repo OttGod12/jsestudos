66d4c78f29fad0a4cf858401bac2b5711c8e8ed9
OttGod12 <130059670+OttGod12@users.noreply.github.com>
2024-05-15 11:57:07 -0300

    .

--- geradorlink/script.js
+++ geradorlink/script.js
@@ -1,13 +1,47 @@
 const btnCopiar = document.querySelector("[btnCopiar]");
 const btnMandar = document.querySelector("[btnMandar]");
 const numeroTel = document.querySelector("[numeroTel]");
-const resultado = document.querySelector("[inputResult]");
+const p = document.querySelector("[p]")
+const resultado = document.querySelector("[inputResult]")
+const divResult = document.querySelector("[divResultado]")
 
 
-btnCopiar.addEventListener("click", () => {
-    console.log("copiar")
-  });
+const copiar  = (evento) => {
+  divResult.style = "display: block;"
+  resultado.value = "https://wa.me/55" + numeroTel.value
 
-btnMandar.addEventListener("click", () => {
-    console.log("mandar")
-  });
+} 
+
+
+const mandar  = (evento) => {
+  window.open( "https://wa.me/55" + numeroTel.value)
+  
+} 
+
+
+
+const campos = (evento) => {
+  numeroTel.value = numeroTel.value.replace(/\D/g, "")
+  if(numeroTel.value.length == 11){
+    numeroTel.value = numeroTel.value.replace(/(\d{2})(\d)/, "($1) $2" )
+    numeroTel.value = numeroTel.value.replace(/(\d)(\d{4})$/, "$1-$2" )
+
+  }
+  
+} 
+
+
+
+const txtResultado  = (evento) => {
+  navigator.clipboard.writeText("https://wa.me/55" + numeroTel.value)
+  p.innerText = "Link copiado para a área de tranferência"
+} 
+
+
+
+
+
+btnCopiar.addEventListener("click", copiar)
+btnMandar.addEventListener("click", mandar)
+numeroTel.addEventListener("input", campos)
+resultado.addEventListener("click", txtResultado)
